53d3d0f521eca9e7ab971c8bc670d71e36233c5e
Chuan <chuan7901@gmail.com>
2024-09-25 23:51:47 +0000

    first remote push

---
+++ day_1.py
@@ -0,0 +1,28 @@
+#list
+import math
+[1,2,3,5,5]
+['1', '2', 'asldif']
+
+
+#dictionary
+
+{
+    "firstname" : "shawn",
+    "lastname" : "chen"
+}
+
+#tuples they are immutable cannot be changed after created
+("hi" , "this" , "is", "a", "tuple")
+
+
+#set cannnot have repeats 
+{1, 3, 4}
+
+
+#checking data type 
+
+print(type(("hi", "this")))
+
+#Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+#Find an Euclidian distance between (2, 3) and (10, 8)
+print(math.sqrt((10-2)**2+(8-3)**2))
